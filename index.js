cabfece46ad8c5696683664eebeaa3906867b9b3
kaykypg7 <kaykysebastiao35@gmail.com>
2023-12-01 21:39:02 -0300

    second commit

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const nome = document.querySelector("#tName")
 nome.style.backgroundColor = 'blue'
 nome.style.textAlign = 'center'
 
+
+
+
+
 //2
 const inputTxt = document.querySelector(".inpText")
 const btnSub = document.querySelectorAll(".btnEnviar")[0]
 btnSub.addEventListener('click', ()=>{
         console.log(inputTxt.value)
 })
+
+
+
+
 // 3
 const inputVelocidade = document.querySelector(".inpVelo")
 const btnEnviar = document.querySelectorAll(".btnEnviar")[1]
@@ -28,7 +36,55 @@ btnEnviar.addEventListener('click', ()=>{
 
 
 
+// 4
+const fav = document.querySelectorAll('.esporte')[1]
+fav.style.backgroundColor = 'yellow'
 
 
+
+// 5
+
+const inp1 = document.querySelectorAll('.inpSoma')[0]
+const inp2 = document.querySelectorAll('.inpSoma')[1]
+const btnSoma = document.querySelector('#btnCalc')
+const pSoma = document.querySelector('#txtSoma')
+
+btnSoma.addEventListener('click', ()=>{
+    let valorInput1 = Number(inp1.value)
+    let valorInput2= Number(inp2.value)
+    var result = valorInput1 + valorInput2
+    pSoma.innerHTML = `<b>o resultado é ${result}<b>`
+    
+})
+
+
+
+
+// 6
+
+// deve ter uma maneira mais fácil mas não veio na minha cabeça kkkkk
+var nomes = []; 
+var input = document.querySelector('.inp')
+const btn = document.querySelector('.btn')
+var caixa = document.getElementById('caixa')
+
+    btn.addEventListener("click", (e)=>{
+        var valor_input = input.value
+        nomes.push(valor_input); 
+        console.log("Nomes digitados:", nomes) // teste
+        caixa.innerHTML = '<h1>Nomes digitados</h1>'
+        for(i = 0; i< nomes.length; i++){
+        caixa.innerHTML += `<p>${nomes[i]}</p>`
+        }
+    })
+
+
+    // 7
+
+const pQ7 = document.querySelectorAll('.pQuestao7')
+for (let i = 0; i < pQ7.length; i++) {
+    pQ7[i].style.backgroundColor = 'yellow'
+
+}
 
 
